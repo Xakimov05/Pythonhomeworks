ea0dcb4dab3fe75e3bf1bf20018d714b68e0c44e
Xakimov05 <150587122+Xakimov05@users.noreply.github.com>
2026-07-01 06:06:19 -0400

    Add files via upload

---
+++ lesson-1answer.py
@@ -0,0 +1,29 @@
+# 1. Why do you think companies analyze large volumes of data?
+print("1. There are tons of data in every company which need to be analyzed")
+print("to make better decisions about production, income and outcome,")
+print("and to increase profits.") 
+
+# 2. How can Python help?
+print("2. Python helps to analyze sales, customers, profits and risks on business")
+print("it also helps the business work automatically with creating reports of whole process")
+print("and to make financial decisions to track income, outcome and profits")
+
+# 3. How would you analyze 10,000 customer transactions?
+print("3. First, I will collect all data about them")
+print("learn behavour of the customers about the product")
+print("visualize all data on data charts")
+
+# 4. Python in BI processes
+print("4. Python helps (BI) Business Intelligence to remove errors")
+print("visualize data on charts and give reports about data")
+print("and helps to learn whole process how the business performimg")
+      
+# 5. Comparing company profit year by year
+print("5. Fristly, all data from pruduction is gathered in order to make better decisons")
+print("then visualize all data from income, outcome and profits on charts")
+print("so those data charts help to outcome total profit and compare it with previous years")
+      
+# 6. Difficulties without Python
+print("6. So python helps to generate large date into smaller and understandable visualization")
+print("without it, companies spend months and years to get outcome from whole process of company performance")
+print("and also performance of decision making can be misleading and generate some errors")
